ecc77b97e1828d5b4fcf35ef390d3eb1250d1820
Velichko Stoev <veli4kostoev@gmail.com>
2015-10-13 01:40:05 +0300

    User profile page added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar,
+                    styles: { medium: '300x300', thumb: '100x100' },
+                    default_url: ':style/no_photo.png'
+  validates_attachment_content_type :avatar, content_type: %r{/\Aimage\/.*\Z/}
 
   def full_name
     "#{first_name} #{last_name}"
