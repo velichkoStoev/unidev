b93f5b6eff1eade968b0bee5f695d465c16f4eec
Velichko Stoev <veli4kostoev@gmail.com>
2015-10-13 19:02:28 +0300

    Styling the Profile page and displaying the user's projects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @projects = Project.select('name, projects.created_at, role, is_creator')
+                .joins(:project_participations).where('user_id = ?', @user.id)
   end
 end
-
-#permiti
