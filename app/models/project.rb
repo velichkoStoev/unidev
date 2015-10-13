ff7a56a89645624751ce931f9a736065aef47257
Velichko Stoev <veli4kostoev@gmail.com>
2015-10-13 17:48:18 +0300

    Validations in Project and Project Participations models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :project_participations
   has_many :users, through: :project_participations
+
+  validates :name, presence: true
+  validates :information, presence: true
 end
--- app/models/project_participation.rb
+++ app/models/project_participation.rb
@@ -1,4 +1,8 @@
 class ProjectParticipation < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  validates :project, presence: true
+  validates :user, presence: true
+  validates :is_creator, presence: true
 end
