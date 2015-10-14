f57dea6a35ce51828e637035d8d6f8ebef0dd101
Velichko Stoev <veli4kostoev@gmail.com>
2015-10-14 18:42:01 +0300

    Editing profile view updated - more fields and avatar upload/delete

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   private
 
+  # Redirect to user profile after editing the profile
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+
   # We override the default Devise methods 'sign_up_params'
   # and 'account_update_params' in order to add our custom fields
 
@@ -11,8 +16,8 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :faculty,
+    params.require(:user).permit(:avatar, :first_name, :last_name, :faculty,
                                  :email, :password, :password_confirmation,
-                                 :current_password)
+                                 :current_password, :delete_avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,21 @@ class User < ActiveRecord::Base
   has_many :project_participations
   has_many :projects, through: :project_participations
 
+  attr_accessor :delete_avatar
+
   has_attached_file :avatar,
-                    styles: { medium: '300x300', thumb: '100x100' },
+                    styles: { medium: '300x300', thumb: '200x200' },
                     default_url: ':style/no_photo.png'
-  validates_attachment_content_type :avatar, content_type: %r{/\Aimage\/.*\Z/}
+
+  validates_attachment :avatar, content_type: {
+    content_type: ['image/jpeg', 'image/png']
+  }
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :faculty, presence: true
+
+  before_validation { avatar.clear if delete_avatar == '1' }
 
   def full_name
     "#{first_name} #{last_name}"
