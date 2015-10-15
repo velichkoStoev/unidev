3faf68b1c65944071ccbb7c413aa9fbda89dac69
Velichko Stoev <veli4kostoev@gmail.com>
2015-10-15 15:15:25 +0300

    Dashboard added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   resources :users
+
+  get '/dashboard', to: 'pages#dashboard'
+
   root 'pages#home'
 end
