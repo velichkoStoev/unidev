5f566b04f484e78744bf1a0315c728c2d138fce8
Velichko Stoev <veli4kostoev@gmail.com>
2015-10-25 22:11:42 +0200

    Adding skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,37 @@
 class SkillsController < ApplicationController
   def index
-    @skills = User.find(params[:user_id]).skills
+    @user = User.find(params[:user_id])
+    @skills = @user.skills
+    @new_skill = Skill.new
+  end
+
+  def create
+    @skill = Skill.find_by_name(skill_params[:name])
+
+    # TODO: Implement error handling here!
+
+    if @skill
+      create_user_skill
+    else
+      @skill = current_user.skills.build(skill_params)
+      respond_to do |format|
+        if @skill.save
+          create_user_skill
+          format.js
+        else
+          format.js
+        end
+      end
+    end
+  end
+
+  private
+
+  def create_user_skill
+    UserSkill.create(skill: @skill, user: current_user)
+  end
+
+  def skill_params
+    params.require(:skill).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   resources :users do
-    resources :skills, only: [:index]
+    resources :skills, only: [:index, :create]
   end
 
   get '/dashboard', to: 'pages#dashboard'
