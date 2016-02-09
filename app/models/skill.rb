63a6b386aebd4d125022ad9311fb0676439e8f40
Velichko Stoev <veli4kostoev@gmail.com>
2016-02-09 22:35:14 +0100

    Add uniqueness constraint on skill name

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,5 +2,5 @@ class Skill < ActiveRecord::Base
   has_many :user_skills
   has_many :skills, through: :user_skills
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
