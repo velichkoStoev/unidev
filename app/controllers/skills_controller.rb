84a719798d092065a683aadeb0ed26a84b40cb3f
Velichko Stoev <veli4kostoev@gmail.com>
2016-02-09 22:35:39 +0100

    Skills 'create' action refactored

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,35 +1,14 @@
 class SkillsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @skills = @user.skills
-    @new_skill = Skill.new
-  end
-
   def create
-    @skill = Skill.find_by_name(skill_params[:name])
-
-    # TODO: Implement error handling here!
-
-    if @skill
-      create_user_skill
-    else
-      @skill = current_user.skills.build(skill_params)
-      respond_to do |format|
-        if @skill.save
-          create_user_skill
-          format.js
-        else
-          format.js { render 'create_error' }
-        end
-      end
+    @skill = current_user.skills.create!(skill_params)
+    respond_to { |format| format.js }
+  rescue ActiveRecord::RecordInvalid
+    respond_to do |format|
+      format.js { render 'create_error' }
     end
   end
 
   private
-
-  def create_user_skill
-    UserSkill.create(skill: @skill, user: current_user)
-  end
 
   def skill_params
     params.require(:skill).permit(:name)
