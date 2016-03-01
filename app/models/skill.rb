62b382cd2ced7f1d8455848f98812ac7267dee88
Velichko Stoev <veli4kostoev@gmail.com>
2016-03-01 01:46:24 +0100

    Update the checks for the skill name

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,5 +2,5 @@ class Skill < ActiveRecord::Base
   has_many :user_skills
   has_many :skills, through: :user_skills
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
