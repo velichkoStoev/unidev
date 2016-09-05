fac919b019d64d7576db34ea77252d4ab7a44158
Velichko Stoev <veli4kostoev@gmail.com>
2016-09-05 18:53:54 +0300

    Add view helpers for user authorization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def user_same_as_current?
+    @user.id == current_user.id
+  end
+
+  def user_different_than_current?
+    @user.id != current_user.id
+  end
+
+  def user_project_creator?
+    @project.creator.id = @user.id
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,2 @@
 module UsersHelper
-  def user_same_as_current?
-    @user.id == current_user.id
-  end
 end
