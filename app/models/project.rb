72cd529b7b90a9bd8030ece5eac5f5e7f6f5b6e3
Velichko Stoev <veli4kostoev@gmail.com>
2016-09-05 18:55:33 +0300

    Update Project model
    Validate the presence of the creator and dependent-destory the related ProjectParticiption records

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,12 @@
 class Project < ActiveRecord::Base
-  has_many :project_participations
+  has_many :project_participations, dependent: :destroy
   has_many :users, through: :project_participations
 
   belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
 
   validates :name, presence: true
   validates :information, presence: true
+  validates :creator, presence: true
 
   def date_created
     created_at.localtime.strftime('%d %B %Y')
