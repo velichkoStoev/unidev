1f5154a3ccfe92e4f352150e372a901feb772332
Velichko <veli4kostoev@gmail.com>
2016-11-15 17:37:11 +0200

    Fix mistyped association

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
   has_many :user_skills
-  has_many :skills, through: :user_skills
+  has_many :users, through: :user_skills
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
