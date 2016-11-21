3bbe242c97a1acf66ff5e3d052d88c3d1b07968f
Velichko Stoev <veli4kostoev@gmail.com>
2016-11-21 15:09:09 +0200

    Add more Messages features

--- spec/features/messages_features_spec.rb
+++ spec/features/messages_features_spec.rb
@@ -31,21 +31,45 @@ describe 'Messages Features' do
     scenario { expect(page).to have_link('Compose new message', href: new_user_message_path(current_user.id)) }
   end
 
-  # feature 'User reads a message' do
-  #   let!(:received_message) { FactoryGirl.create(:message, receiver: current_user) }
+  feature 'User reads a message', js: true do
+    let!(:received_message) { FactoryGirl.create(:message, receiver: current_user) }
 
-  #   before do
-  #     login_as(current_user, scope: :user)
-  #     visit user_messages_path(current_user.id)
-  #     click_link(received_message.title)
-  #   end
+    before do
+      login_as(current_user, scope: :user)
+      visit user_messages_path(current_user.id)
+      click_link(received_message.title)
+    end
 
-  #   scenario 'they see the content of the message' do
-  #     expect(page).to have_selector('.message .panel.panel-heading', text: 'Message')
-  #     expect(page).to have_selector('.container-fluid .list-group-item', text: received_message.title)
-  #     expect(page).to have_selector('.container-fluid .list-group-item', text: received_message.body)
-  #   end
-  # end
+    scenario 'they see the content of the message' do
+      expect(page).to have_selector('.message .panel.panel-heading', text: 'Message')
+      expect(page).to have_selector('.container-fluid .list-group-item', text: received_message.title)
+      expect(page).to have_selector('.container-fluid .list-group-item', text: received_message.body)
+    end
 
-  feature 'User composes a message'
+    context 'when the user has unread messages' do
+      scenario { expect(page).to have_selector('tr.warning') }
+    end
+
+    context 'when the user has no unread messages' do
+      scenario { expect(page).not_to have_selector('tr.warning') }
+    end
+  end
+
+  feature 'User composes a message', js: true do
+    before do
+      login_as(current_user, scope: :user)
+      visit user_messages_path(current_user.id)
+      click_link('Compose new message')
+    end
+
+    scenario 'they see a form for composing a new message' do
+      expect(page).to have_selector('.message_title label.control-label', text: 'Title')
+      expect(page).to have_selector('.message_title input')
+      expect(page).to have_selector('.message_body label.control-label', text: 'Body')
+      expect(page).to have_selector('.message_body textarea')
+      expect(page).to have_selector('.message_receiver_id label.control-label', text: 'Receiver')
+      expect(page).to have_selector('.message_receiver_id select')
+      expect(page).to have_button('Sent')
+    end
+  end
 end
